5603c6461fdf4f56f1e71c6fb2ef31b4de8def68
Kristof_Rakoczi <kristof_rakoczi1@epam.com>
2019-05-11 18:25:54 +0200

    Play component contains the playable games, filled with dummy "gamecards" for now.

---
+++ mario-needed-money/src/app/play/play.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { stringify } from '@angular/core/src/render3/util';
+
+@Component({
+	selector: 'app-play',
+	templateUrl: './play.component.html',
+	styleUrls: ['./play.component.css']
+})
+export class PlayComponent implements OnInit {
+
+	  playerName: string;
+
+	constructor() {
+		this.playerName = JSON.parse(localStorage.getItem("name"));
+   		console.log("Player's name: %s", this.playerName);
+  	}
+
+	ngOnInit() {
+	}
+
+}
