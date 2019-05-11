d99c54447f73df89bff9e5deb0c5331651d568f5
Kristof_Rakoczi <kristof_rakoczi1@epam.com>
2019-05-11 16:11:44 +0200

    Setup routing /start, and index.

--- mario-needed-money/src/app/app.module.ts
+++ mario-needed-money/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule} from './material';
+import { StartComponent } from './start/start.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- mario-needed-money/src/app/material.ts
+++ mario-needed-money/src/app/material.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
-    imports: [MatButtonModule],
-    exports: [MatButtonModule],
+    imports: [MatButtonModule, MatToolbarModule, MatIconModule],
+    exports: [MatButtonModule, MatToolbarModule, MatIconModule],
   })
   export class MaterialModule { }
